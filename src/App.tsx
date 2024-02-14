30ecd19118c1087f0169e495d25cf2626f6660e0
anas <anas38.gt@gmail.com>
2024-02-14 11:51:40 +0100

    début onclick ok

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,18 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  const handleClick = (key: string) => {
+      console.log(key+" Choisi !")
+  }
 
   return (
     <div className='container'>
       <div className='container-game'>
-        <div className='btnJeu' id='red'></div>
-        <div className='btnJeu' id='blue'></div>
-        <div className='btnJeu' id='green'></div>
-        <div className='btnJeu' id='yellow'></div>
+        <div className='btnJeu' id='red' onClick={() => handleClick("red")}></div>
+        <div className='btnJeu' id='blue' onClick={() => handleClick("blue")}></div>
+        <div className='btnJeu' id='green' onClick={() => handleClick("green")}></div>
+        <div className='btnJeu' id='yellow' onClick={() => handleClick("yellow")}></div>
       </div>
       <button id='btnStart'>START</button>
     </div>
