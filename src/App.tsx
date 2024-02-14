2d740895dd4f2e9fb7e4a5fa2285cb22701133ec
anas <anas38.gt@gmail.com>
2024-02-14 14:46:22 +0100

    jeu fonctionne sans phase automatique

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import { useState } from 'react'
+import { useMemo, useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  
-  const handleClick = (key: string) => {
-      console.log(key+" Choisi !")
+  const [sequence, setSequence] = useState<number[]>([])
+  const [start, setStart] = useState(false)
+  const [click, setClick] = useState(0)
+
+  const gameOver = useMemo(() => click === -1, [click])
+
+  useEffect(() => {
+    console.log(sequence)
+  }, [sequence]);
+
+  useEffect(() => {
+    if (gameOver) {
+      setSequence([])
+      setStart(false)
+      setClick(0)
+    }
+  }, [gameOver]);
+
+  const startSequence = () => {
+    let newSequence = sequence.slice()
+    newSequence.push(Math.floor(Math.random() * 4) + 1);
+    setSequence(newSequence)
+    setClick(0)
+    setStart(true)
+  }
+
+  const handleClick = (key: number) => {
+    if(key == sequence[click]){
+      if(click === sequence.length - 1){
+        console.log('sequence complétér')
+        startSequence()
+      }
+      else {
+        console.log("ok on avance")
+        setClick(click+1)
+      }
+    }
+    else{
+      console.log('erreur dans la sequence')
+      setClick(-1)
+    }
   }
 
   return (
     <div className='container'>
       <div className='container-game'>
-        <div className='btnJeu' id='red' onClick={() => handleClick("red")}></div>
-        <div className='btnJeu' id='blue' onClick={() => handleClick("blue")}></div>
-        <div className='btnJeu' id='green' onClick={() => handleClick("green")}></div>
-        <div className='btnJeu' id='yellow' onClick={() => handleClick("yellow")}></div>
+        <div className='btnJeu' id='red' onClick={() => handleClick(1)}></div>
+        <div className='btnJeu' id='blue' onClick={() => handleClick(2)}></div>
+        <div className='btnJeu' id='green' onClick={() => handleClick(3)}></div>
+        <div className='btnJeu' id='yellow' onClick={() => handleClick(4)}></div>
       </div>
-      <button id='btnStart'>START</button>
+      {!start && <button id='btnStart' onClick={() => startSequence()}>START</button>}
     </div>
   )
 }
